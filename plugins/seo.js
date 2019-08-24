cd7af7340312dc55225bdba86ed865df31b1a528
Alexander Smith <acsmith.dev@gmail.com>
2019-08-24 13:24:54 -0400

    update seo and reinstall packages

--- plugins/seo.js
+++ plugins/seo.js
@@ -2,24 +2,28 @@ export default ({ store }, inject) => {
   // General SEO
   inject('generalSeo', () => {
     return {
-      title: store.site.title,
+      title: store.state.site.title,
       meta: [
         {
           name: 'description',
           hid: 'description',
-          content: store.site.description
+          content: store.state.site.description
         },
-        { name: 'keywords', hid: 'keywords', content: store.site.keywords },
-        { name: 'og:title', content: store.site.title },
-        { name: 'og:description', content: store.site.description },
+        {
+          name: 'keywords',
+          hid: 'keywords',
+          content: store.state.site.keywords
+        },
+        { name: 'og:title', content: store.state.site.title },
+        { name: 'og:description', content: store.state.site.description },
         { name: 'og:type', content: 'website' },
-        { name: 'og:url', content: store.site.url },
-        { name: 'og:image', content: store.site.image },
+        { name: 'og:url', content: store.state.site.url },
+        { name: 'og:image', content: store.state.site.image },
         { name: 'twitter:card', content: 'summary' },
-        { name: 'twitter:site', content: store.socials.twitter.username },
-        { name: 'twitter:title', content: store.site.title },
-        { name: 'twitter:description', content: store.site.description },
-        { name: 'twitter:image', content: store.site.image },
+        { name: 'twitter:site', content: store.state.socials.twitter.username },
+        { name: 'twitter:title', content: store.state.site.title },
+        { name: 'twitter:description', content: store.state.site.description },
+        { name: 'twitter:image', content: store.state.site.image },
         { name: 'twitter:image:alt', content: 'Twitter Image Alt' }
       ]
     }
