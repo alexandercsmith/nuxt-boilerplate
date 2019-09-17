e23d113775622b7a8f93990b4a4df7150b9e90b8
Alexander Smith <acsmith.dev@gmail.com>
2019-09-16 20:44:54 -0400

    update state

--- store/state.js
+++ store/state.js
@@ -8,11 +8,31 @@ export default () => ({
     email: ''
   },
   socials: {
-    facebook: { username: '', url: '', icon: 'facebook-square' },
-    instagram: { username: '', url: '', icon: 'instagram' },
-    twitter: { username: '', url: '', icon: 'twitter-square' },
-    youtube: { username: '', url: '', icon: 'youtube-square' },
-    github: { username: '', url: '', icon: 'github-square' }
+    facebook: {
+      username: '', 
+      url: '', 
+      icon: 'facebook-square' 
+    },
+    instagram: {
+      username: '', 
+      url: '', 
+      icon: 'instagram' 
+    },
+    twitter: {
+      username: '', 
+      url: '', 
+      icon: 'twitter-square' 
+    },
+    youtube: {
+      username: '', 
+      url: '', 
+      icon: 'youtube-square' 
+    },
+    github: {
+      username: '', 
+      url: '', 
+      icon: 'github-square' 
+    }
   },
   blog: {
     articles: [],
